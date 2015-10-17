55a0a3b516bde5b61a8b96b7a139d96c1ff76835
Ryan Steven Behrens <rbehrens01@yahoo.com>
2015-10-16 19:56:40 -0700

    final

--- server.js
+++ server.js
@@ -1,40 +1,73 @@
 var express = require("express");
 var app = express();
-
+var bodyparser = require('body-parser');
 var port = process.env.PORT || 3001;
 
 
+var Adjective = require('./lib/adjective.js');
+var Noun = require('./lib/noun.js');
+var Verb = require('./lib/verb.js');
+
+var getRandomWord = require('./lib/getrandomword.js');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static(__dirname + '/app/'));
 
 
-
-
-
-
-var Adjective = function() {
- this.Hungry = true;
- this.Crazy = true;
- this.PMSing = true;
- this.Lazy = true;
- this.Amazing = true;
-};
 
 var adjective = new Adjective();
 
-function getRandomWord (object) {
- var propArray = Object.keys(object);
-  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
-  return {word: randomProp};
-}
 
+var verb = new Verb();
+
+var noun = new Noun();
+
+function postWord (word, wordObject) {
+  if (wordObject.hasOwnProperty(word)) {
+    return {msg: 'We already have your awesome word, ' + word + ', in our list.'};
+  }
+
+  wordObject[word] = true;
+  console.log(wordObject);
+
+  return {msg: 'Thanks for submitting ' + word + '!'};
+};
 
 app.get("/", function (req,res){
-  res.send("Hello Universe yes");
+   res.sendFile("index.html");
+});
+
+app.get("/verb", function (req,res){
+  res.json(getRandomWord(verb));
+});
+
+app.get("/noun", function (req,res){
+  res.json(getRandomWord(noun));
 });
 
 app.get("/adjective", function (req,res){
   res.json(getRandomWord(adjective));
 });
 
+
+app.post("/adjective", function (req,res){
+  var word = postWord(req.body.word, adjective);
+  res.json(word);
+});
+
+app.post("/verb", function (req,res){
+  var word = postWord(req.body.word, verb);
+  res.json(word);
+});
+
+app.post("/noun", function (req,res){
+  var word = postWord(req.body.word, noun);
+  res.json(word);
+});
 
 
 app.listen(port, function(){
